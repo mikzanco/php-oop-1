dfbb7190e0522160475f7a2bd4538dec2a3dde78
mikzanco <mikyzancolo97@gmail.com>
2022-12-07 17:05:20 +0100

    add new function per calcolare e scrivere in pagina quanti giorni sono passati o mancano all'uscita del film

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-$film1 = new Movie("John Wick","22 gen 2015", "Chad Stahelski", "Keanu Reeves");
-$film2 = new Movie("Avatar 2", "14 dic 2022", "James Cameron", "Sam Worthington");
+$film1 = new Movie("John Wick","22-01-2014", "Chad Stahelski", "Keanu Reeves");
+$film2 = new Movie("Avatar 2", "14-12-2022", "James Cameron", "Sam Worthington");
 
 $film1->setImg("https://www.sentieridelcinema.it/wp-content/uploads/2016/03/EM7mrES6qpFJMPfNufC4Of8jVuMmZYOkdcv7.jpg");
 $film2->setImg("https://i0.wp.com/www.badtaste.it/cinema/wp-content/uploads/sites/1/2022/11/avatar-2-la-via-dellacqua-poster.jpg?fit=1200%2C1501&quality=85&strip=all&ssl=1");
@@ -32,7 +32,19 @@ class Movie{
 
     // creo una funzione per calcolare da quanti giorni è uscito il film
     public function dayFrom(){
-
+        
+        // $datetime1 = date_create('17-09-2018');
+        // $datetime2 = date_create('25-09-2018');
+        $dateFilm = date_create($this->date);
+        $dateNow = date_create('now');
+  
+        // Calculates the difference between DateTime objects
+        $interval = date_diff($dateNow, $dateFilm);
+        
+        // Display the result
+        echo $interval->format(' %R%a days ');
+        
+        
     }
 
     public function setImg($_img){
@@ -49,9 +61,10 @@ class Movie{
         return $placeholder;
     }
 }
-// var_dump($filmList);
+
 
 ?>
+
 
 
 <!DOCTYPE html>
@@ -70,6 +83,7 @@ class Movie{
             <tr>
             <th scope="col">Titolo</th>
             <th scope="col">Data di Uscita</th>
+            <th scope="col">Giorni passati o mancanti all'uscita del film</th>
             <th scope="col">Poster</th>
             <th scope="col">Autore</th>
             <th scope="col">Protagonista</th>
@@ -78,8 +92,10 @@ class Movie{
         <tbody>
             <?php foreach($filmList as $film) : ?>
                 <tr>
+                    <?php $film->dayFrom(); ?>
                     <td><?php echo $film->title ?></td>
                     <td><?php echo $film->date ?></td>
+                    <td><?php echo $film->dayFrom() ?></td>
                     <td><img class="img-thumbnail" src="<?php echo $film->getImg() ?>"></td>
                     <td><?php echo $film->author ?></td>
                     <td><?php echo $film->leadingActor ?></td>
